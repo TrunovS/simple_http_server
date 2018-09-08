5d83dd5fd35f53aa0e2c3e8634852f3b79e6b17a
Trunov Sergey <trunovsergey@list.ru>
2018-09-08 23:50:41 +0300

    Ошибка чтения ответов.

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,23 @@ fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer));
+    // println!("Request: {}", String::from_utf8_lossy(&buffer));
 
-    let (status_line, filename) = if buffer.starts_with(b"GET / HTTP/1.1") {
-        ("HTTP/1.1 200 OK\r\n\r\n", "../html/hello.html")
+    let (status_line, filename) = if buffer.starts_with(b"GET /hello HTTP/1.1") {
+        ("HTTP/1.1 200 OK\r\n\r\n", "html/hello.html")
     } else if buffer.starts_with(b"GET /sleep HTTP/1.1\r\n") {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "../html/hello.html")
+        thread::sleep(Duration::from_secs(10));
+        ("HTTP/1.1 200 OK\r\n\r\n", "html/hello.html")
     }else {
-            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "../html/404.html")
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "html/404.html")
     };
 
-    let mut file = File::open(filename).unwrap();
+    let mut file = File::open(filename).expect("Html response file is not opened");
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let mut response = format!("{}{}", status_line,contents);
 
-    println!("Response: {}", response);
+    // println!("Response: {}", response);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
@@ -40,8 +40,9 @@ fn main() -> io::Result<()> {
     let pool = ThreadPool::new(4);
 
     for stream in lstener.incoming() {
+        let stream = stream.unwrap();
         pool.execute(|| {
-            handle_client(stream.unwrap());
+            handle_client(stream);
         });
     }
     Ok(())
