46cd73cfc65bcaefc098f185b552e3ce21e9e0ae
Trunov Sergey <trunovsergey@list.ru>
2018-09-15 20:49:14 +0300

    Возвращаем результат и обрабатываем возможную ошибку.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::net::{TcpListener, TcpStream };
 use std::io::prelude::*;
-use std::io;
 use std::fs::File;
 use std::thread;
 use std::time::Duration;
 
 extern crate thread_pool;
 use thread_pool::ThreadPool;
+use thread_pool::PoolError;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -26,7 +26,7 @@ fn handle_client(mut stream: TcpStream) {
     let mut file = File::open(filename).expect("Html response file is not opened");
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let mut response = format!("{}{}", status_line,contents);
+    let response = format!("{}{}", status_line,contents);
 
     // println!("Response: {}", response);
 
@@ -34,16 +34,20 @@ fn handle_client(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
-fn main() -> io::Result<()> {
-    let lstener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+fn main() -> Result<(),PoolError> {
     let pool = ThreadPool::new(4);
-
-    for stream in lstener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| {
-            handle_client(stream);
-        });
+    match pool {
+        Err(pool_err) => { return Err(pool_err); },
+        Ok(pool) => {
+            let lstener = TcpListener::bind("127.0.0.1:7878").unwrap();
+            for stream in lstener.incoming().take(3) {
+                let stream = stream.unwrap();
+                pool.execute(|| {
+                    handle_client(stream);
+                });
+            }
+        }
     }
-    Ok(())
+    return Ok(());
+
 }
